daa2e860a1bb2a46cab34d19498f2f3b387c31f8
samcorintis <samuel.russo@corintis.com>
2024-03-01 14:18:23 +0100

    better coeffs

--- stokes.py
+++ stokes.py
@@ -7,15 +7,16 @@ from scipy.spatial import KDTree
 # import matplotlib.pyplot as plt
 from helpers import (
     alpha,
-    calculate_diffusion_coeff_channel_design,
-    calculate_heat_transfer_coeff_channel_design,
-    calculate_heat_transfer_coeff_substrate,
-    calculate_heat_transfer_effective,
+    kappa_interpolation,
     mesh_size,
     ramp_interpolation,
     wall_drag_term,
 )
 from pyMMAopt import ReducedInequality, MMASolver
+
+
+pvd_frequency = 5
+hdf5_frequency = 5
 
 # --- 0. Parameters Initialisation ---
 # 0.1 Mesh and scales
@@ -28,10 +29,13 @@ Ly = 1.0  # non-dim
 mesh = fd.RectangleMesh(Nx, Ny, Lx, Ly)
 
 # 0.2 Physical Data
-channel_thickness = 380e-6
+# temperature at inlet is 20C
+T_inlet = 293.15 # K
+
+channel_thickness = 380e-6 # m
 Ht = 0.5 * channel_thickness
 
-substrate_thickness = 150e-6
+substrate_thickness = 150e-6 # m
 Hs = 0.5 * substrate_thickness
 
 conductivity_fluid = 0.598
@@ -40,15 +44,20 @@ viscosity_fluid = 1.004e-3
 capacity_fluid = 4180
 density_fluid = 998
 
-applied_pressure = 10000  # kg m^-1 s^-2
+applied_pressure = 100000 * 5.0 / 4.0  # kg m^-1 s^-2
 L = 0.001  # m
 U = np.sqrt(applied_pressure / density_fluid)  # m/s
-print(U)
 
-Re = L * U * density_fluid / viscosity_fluid
-prandtl_number = fd.Constant(capacity_fluid * viscosity_fluid / conductivity_fluid)
+
+nu = viscosity_fluid / density_fluid
+
+Re = L * U / nu
+Prandtl_number = fd.Constant(capacity_fluid * viscosity_fluid / conductivity_fluid)
 nondim_Ht = Ht / L
 nondim_Hs = Hs / L
+
+print("Reynolds number: ", Re)
+print("Prandtl number: ", Prandtl_number)
 
 
 # 0.3 Optimisation Parameters
@@ -62,8 +71,8 @@ ramp_p_thermal_values = np.linspace(40.0, 310.0, STAGES)
 p_value = 8
 
 Htfactor_value = 2e2
-initial_rho_value = 0.1
-cost_function_scale = 100.0
+initial_rho_value = 0.5
+cost_function_scale = 1.0
 
 parameters_mma = {
     "move": 0.2,
@@ -101,11 +110,11 @@ with stop_annotating():
 # random square moving around (one or two squares)
 power_map_data = np.zeros((Nx, Ny))
 for i in range(int(Nx / 3), int(4 * Nx / 5)):
-    power_map_data[i, int(2 * Ny / 5)] = 1.0
-    power_map_data[i, int(4 * Ny / 5)] = 1.0
+    power_map_data[i, int(2 * Ny / 5)] = 5e5
+    power_map_data[i, int(4 * Ny / 5)] = 5e5
 
 for i in range(int(Nx / 5), int(3 * Nx / 5)):
-    power_map_data[i, int(Ny / 2)] = 1.0
+    power_map_data[i, int(Ny / 2)] = 5e5
 
 POWER_MAP = fd.FunctionSpace(mesh, "DG", 0)
 power_map_f = fd.Function(POWER_MAP)
@@ -163,26 +172,21 @@ vq = fd.TestFunction(UP)
 (v, q) = fd.split(vq)
 
 # Define viscosity and bcs
-nu = fd.Constant(1.0 / Re)
-
 p0 = fd.Constant(1.0)  # 1 at left, 0 at right
 bcs = fd.DirichletBC(UP.sub(0), fd.Constant((0, 0)), WALLS)
 n = fd.FacetNormal(mesh)
 
 # Stokes problem
 F_fluid = (
-    fd.inner(2 * nu * fd.sym(fd.grad(u)), fd.sym(fd.grad(v))) * fd.dx
+    fd.inner(2. / Re * fd.sym(fd.grad(u)), fd.sym(fd.grad(v))) * fd.dx
     - fd.div(u) * q * fd.dx
     - fd.div(v) * p * fd.dx
     + p0 * fd.dot(v, n) * fd.ds(INLET)
 )
 
 # Brinkman term
-Ht = 0.5 * channel_thickness
-
-# TODO: save brinkman term as a field
 F_fluid += (
-    nu
+    (1. / Re)
     * (
         wall_drag_term(Ht=Ht, L=L)
         + alpha(rhof, Ht=Ht, ramp_p=ramp_p_fluid, Htfactor=Htfactor, L=L)
@@ -200,44 +204,29 @@ fd.solve(F_fluid == 0, up, bcs)
 # --- 5. Thermal Problem ---
 # 5.1 Thermal Coefficients
 # 5.1.1 For the Channel Layer
-advection_coeff = (24535.0 / 33462.0) * ramp_interpolation(rhof, ramp_p_fluid)
-
-conductivity_channel_coeff = calculate_diffusion_coeff_channel_design(
-    conductivity_fluid, conductivity_substrate, rhof, ramp_p_fluid
+advection_channel_coeff = (
+    (2.0 / 3.0)
+    * ramp_interpolation(rhof, ramp_p_fluid)
+    * density_fluid
+    * capacity_fluid
+    * U
+    / L
 )
 
-diffusion_channel_coeff = (
-    conductivity_channel_coeff / conductivity_fluid * fd.Constant(nu / prandtl_number)
+conductivity_channel = kappa_interpolation(
+    rhof, ramp_p_thermal, k_fluid=conductivity_fluid, k_solid=conductivity_substrate
 )
+diffusion_channel_coeff = (49.0 / 52.0) * conductivity_channel / (L * L)
 
-heat_transfer_coeff_channel_design = calculate_heat_transfer_coeff_channel_design(
-    conductivity_solid=conductivity_substrate,
-    conductivity_fluid=conductivity_fluid,
-    channel_thickness=channel_thickness,
-    rhof=rhof,
-    ramp_p=ramp_p_thermal,
-)
-heat_transfer_coeff_substrate = calculate_heat_transfer_coeff_substrate(
-    conductivity_substrate, 0.5 * substrate_thickness
-)
-heat_transfer_coeff = calculate_heat_transfer_effective(
-    heat_transfer_coeff_channel_design, heat_transfer_coeff_substrate
-)
+h_t = (35.0 * conductivity_channel) / (26.0 * channel_thickness)
+h_b = conductivity_substrate / substrate_thickness
+effective_conductivity = (h_t * h_b) / (h_t + h_b)
 
-coupling_channel_to_bottom_layer = (
-    heat_transfer_coeff
-    * Ht
-    / fd.Constant(2.0 * nondim_Ht**2 * conductivity_fluid)
-    * fd.Constant(nu / prandtl_number)
-)
-
+coupling_channel_to_bottom_layer_coeff = effective_conductivity / (2.0 * Ht)
 
 # 5.1.2 For the die Substrate Layer
-diffusion_die_substrate_coeff = fd.Constant(4.0 / 3.0)
-coupling_die_substrate_to_top_layer = (
-    heat_transfer_coeff * Hs / fd.Constant(2.0 * nondim_Hs**2 * conductivity_substrate)
-)
-
+diffusion_substrate_coeff = conductivity_substrate / (2 * L * L)
+coupling_substrate_to_top_layer_coeff = effective_conductivity / (2.0 * Hs)
 
 Z = fd.FunctionSpace(mesh, "CG", 1)
 T = Z * Z
@@ -248,18 +237,18 @@ tau_channel, tau_substrate = fd.split(tau_total)
 
 # 5.2 Thermal Form
 # 5.2.1 Heat Flux
-F_thermal = -power_map_f * tau_substrate * fd.dx
+F_thermal = -power_map_f / (2 * Hs) * tau_substrate * fd.dx
 # 5.2.2 Die Substrate Layer
 # 5.2.2.1 Die Substrate layer diffusion
 F_thermal += (
-    diffusion_die_substrate_coeff
+    diffusion_substrate_coeff
     * fd.inner(fd.grad(t_substrate), fd.grad(tau_substrate))
     * fd.dx
 )
 
 # 5.2.2.2 Die Substrate layer coupling to Channel layer
 F_thermal += (
-    coupling_die_substrate_to_top_layer
+    coupling_substrate_to_top_layer_coeff
     * (t_substrate - t_channel)
     * tau_substrate
     * fd.dx
@@ -267,26 +256,36 @@ F_thermal += (
 
 # 5.2.3 Channel Layer
 # 5.2.3.1 Channel Layer advection
-F_thermal += advection_coeff * fd.inner(u, fd.grad(t_channel)) * tau_channel * fd.dx
+F_thermal += (
+    advection_channel_coeff
+    * fd.inner(u, fd.grad(t_channel))
+    * tau_channel
+    * fd.dx
+)
 
 # 5.2.3.2 Channel Layer diffusion
 F_thermal += (
-    diffusion_channel_coeff * fd.inner(fd.grad(t_channel), fd.grad(tau_channel)) * fd.dx
+    diffusion_channel_coeff
+    * fd.inner(fd.grad(t_channel), fd.grad(tau_channel))
+    * fd.dx
 )
 
 # 5.2.3.3 Channel Layer coupling to Substrate layer
 F_thermal += (
-    -coupling_channel_to_bottom_layer * (t_substrate - t_channel) * tau_channel * fd.dx
+    -coupling_channel_to_bottom_layer_coeff
+    * (t_substrate - t_channel)
+    * tau_channel
+    * fd.dx
 )
 
 # 5.3 Thermal Boundary Conditions
-bc1 = fd.DirichletBC(T.sub(0), fd.Constant(0.0), INLET)
+bc1 = fd.DirichletBC(T.sub(0), fd.Constant(T_inlet), INLET)
 fd.solve(F_thermal == 0, t_total, bcs=[bc1])
 
 # --- 6. MMA ---
 
 # 6.1 Vizualisation callback
-output_file = fd.File("output/output.pvd")
+pvd_output_file = fd.File("output/output.pvd")
 
 t_total_node = fda.Control(t_total)
 t_total_viz = fd.Function(T)
@@ -308,12 +307,11 @@ rhof_viz.rename("rhof")
 
 global_i = 0
 
-
 @no_annotations
 def output(x, y, z):
     global global_i
     # TODO: save output to hdf5 file
-    if global_i % 10 == 0:
+    if global_i % pvd_frequency == 0:
         _u, _p = up_node.tape_value().split()
         up_viz.sub(0).assign(_u)
         up_viz.sub(1).assign(_p)
@@ -325,7 +323,7 @@ def output(x, y, z):
         rho_viz.assign(rho_node.tape_value())
         rhof_viz.assign(rhof_node.tape_value())
 
-        output_file.write(
+        pvd_output_file.write(
             up_viz.sub(0),
             up_viz.sub(1),
             t_total_viz.sub(0),
@@ -333,10 +331,16 @@ def output(x, y, z):
             rho_viz,
             rhof_viz,
         )
+    if global_i % hdf5_frequency == 0:
+        with fd.CheckpointFile(f"output/results-{global_i}.h5", 'w') as save_file:
+            save_file.save_mesh(mesh)  # optional
+            save_file.save_function(rho_node.tape_value(), name="rho")
+            save_file.save_function(rhof_node.tape_value(), name="rhof")
+            save_file.save_function(up_node.tape_value(), name="up")
+            save_file.save_function(t_total_node.tape_value(), name="t_total")
 
     global_i += 1
     return y
-
 
 # 6.2 Cost function definition
 with stop_annotating():
